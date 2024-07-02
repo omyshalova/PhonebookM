e40d1a6e1e901206441203d8d9bde4accd0ec6d4
Maria Z <z0559882272@gmail.com>
2024-07-02 20:58:34 +0300

    logout

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -46,4 +46,8 @@ public class HelperUser extends HelperBase {
     public boolean isLogged() {
         return isElementPresent(By.xpath("//button[text()='Sign Out']"));
     }
+
+    public void logout() {
+        click(By.xpath("//button[text()='Sign Out']"));
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -2,9 +2,18 @@ package tests;
 
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SIGN OUT present --->logout
+        if (app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
     public void loginSuccess() {
@@ -17,6 +26,14 @@ public class LoginTests extends TestBase {
 //        Assert.assertTrue();
 //        Assert.assertFalse();
 
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
         Assert.assertTrue(app.getHelperUser().isLogged());
     }
 
